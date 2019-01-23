e370f8c4cb521f7521d8eca5cc969b334464c1a2
ethai16 <erick.thai16@gmail.com>
2019-01-23 11:45:32 -0600

    passport and passwords

--- init.bash
+++ init.bash
@@ -5,11 +5,11 @@ sequelize model: generate --name user \
     --attributes username:string,password:string
 
 sequelize model: generate --name mentor \
-    --attributes name:string,email:string,gender:string,industry_id:integer,backgroundCheck:boolean,\
+    --attributes name:string,email:string,gender:string,backgroundCheck:boolean,\
     active:boolean,memo:string
 
 sequelize model: generate --name teacher \
-    --attributes name:string,email:string,gender:string,school_id:integer,telephone:string,memo:string
+    --attributes name:string,email:string,gender:string,telephone:string,memo:string
 
 sequelize model: generate --name student \
     --attributes name:string,grade:integer,school_id:integer,industry_id1:integer,industry_id2:integer,industry_id3:integer,email:string,gender:string,memo:string
--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,35 @@
-const express = require("express");
-
+const express = require('express');
 const router = express.Router();
+const body_parser = require('body-parser')
+const pbkdf2 = require('pbkdf2');
+const crypto = require('crypto');
+const bcrypt = require('bcryptjs');
 
 router.get('/register', (req,res)=>{
     res.render('register')
 })
+
+router.use(body_parser.urlencoded({ extended: false }));
+router.post('/register', (req,res)=>{
+    var username = req.body.username;
+    var pwd = req.body.password;
+    //utilizing bcrypt
+    bcrypt.genSalt(10, (err,salt)=>{
+        console.log(salt)
+        bcrypt.hash(pwd, salt, (err,hash)=>{
+            console.log(hash)
+        })
+    })
+    //requires a res, this bypasses the need to render or send new page.
+    res.status(204).send();
+    
+    //utilizing pbkdf2 and crypto packages
+    // var salt = crypto.randomBytes(256).toString('hex');
+    // var key = pbkdf2.pbkdf2Sync(
+    //     pwd, salt, 36000,256, 'sha256'
+    // );
+    // var hash = key.toString('hex')
+})
+
 
 module.exports = router;
