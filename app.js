08d90b55ee7425f5b20d81bcc1ea192ca7b25a73
Rigo-Hernandez <hernandez_rigoberto@outlook.com>
2019-01-23 15:32:48 -0600

    chat

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -8,7 +11,19 @@ app.use(express.static('public'));
 app.use(require('./routes/index'));
 app.use(require('./routes/register'));
 
+app.get('/', (req, res)=>{
+    // res.sendFile(__dirname + '/views/index.ejs')
+    res.render('index')
+});
 
-app.listen(3000, ()=>{
+
+io.on('connection', (socket)=> {
+    console.log('someone connected')
+    socket.on('chat message', (msg)=> {
+        io.sockets.emit('chat message', msg);
+    });
+});
+
+http.listen(3000, ()=>{
     console.log('listening on port 3000')
 })
