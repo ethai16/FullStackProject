a93a973e6470fa95a0b60a6ffe5736805b21da4c
ethai16 <erick.thai16@gmail.com>
2019-02-04 19:15:54 -0600

    fix

--- app.js
+++ app.js
@@ -44,6 +44,10 @@ io.on('connection', (socket)=> {
 
 
 
-http.listen(3000, ()=>{
-    console.log('listening on port 3000')
-})
+// http.listen(3000, ()=>{
+//     console.log('listening on port 3000')
+// })
+
+http.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
