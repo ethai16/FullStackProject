3add14ad027c6d4b930ad65dc2da35457c2671e7
ethai16 <erick.thai16@gmail.com>
2019-02-05 11:36:41 -0600

    fix

--- models/index.js
+++ models/index.js
@@ -37,22 +37,22 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 // checks if env is Heroku, if so, sets sequelize to utilize the database hosted on heroku
-if (process.env.DATABASE_URL) {
+// if (process.env.DATABASE_URL) {
   // the application is executed on Heroku ... use the postgres database
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect:  'postgres',
-    protocol: 'postgres'
-  })
-}
+//   sequelize = new Sequelize(process.env.DATABASE_URL, {
+//     dialect:  'postgres',
+//     protocol: 'postgres'
+//   })
+// }
 
 // at the bottom of your script, this sets your server to listen for requests, after sequelize has been synced.
 // so if you already have your server listening for requests, maybe delete that code. I think, IDK i just copied this
 // off of documentation
 // db.  is assuming you already set sequelize on db
-db.sequelize.sync().then(function() {
-  http.createServer(app).listen(app.get('port'), function(){
-    console.log('Express server listening on port ' + app.get('port'));
-  });
-});
+// db.sequelize.sync().then(function() {
+//   http.createServer(app).listen(app.get('port'), function(){
+//     console.log('Express server listening on port ' + app.get('port'));
+//   });
+// });
 
 module.exports = db;
