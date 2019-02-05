3c1c95c186a87ab678086f845080e4ee64965c81
ethai16 <erick.thai16@gmail.com>
2019-02-05 13:12:20 -0600

    test2

--- routes/profile.js
+++ routes/profile.js
@@ -15,62 +15,62 @@ const sequelize = new Sequelize('fullstack', 'erickthai', '', {
 
 
 router.get('/:userRole/:username', (req,res)=>{
-    var role = req.params.userRole;
-    var username = req.params.username;
-    var roleNum = ''
-    if (role === 'teacher'){
-        roleNum = 1
-    } else if(role === 'student') {
-        roleNum = 2
-    } else if (role === 'mentor'){
-        roleNum = 3
-    }else{
-        res.redirect('/login')
-    }
+//     var role = req.params.userRole;
+//     var username = req.params.username;
+//     var roleNum = ''
+//     if (role === 'teacher'){
+//         roleNum = 1
+//     } else if(role === 'student') {
+//         roleNum = 2
+//     } else if (role === 'mentor'){
+//         roleNum = 3
+//     }else{
+//         res.redirect('/login')
+//     }
 
-    if (req.user){
-        if (req.user.username === username && roleNum === req.user.role_id) {
-            var masterRole = ""
-            if (req.user.role_id === 1){
-                masterRole = 'teacher'
-            }else if (req.user.role_id === 2){
-                masterRole = 'student' 
-            }else{
-                masterRole = 'mentor'
-            }
-            sequelize.query("SELECT * FROM users INNER JOIN comments ON comments.username = users.username WHERE users.username = '" + username + "' ")
-            .then((results)=>{
+//     if (req.user){
+//         if (req.user.username === username && roleNum === req.user.role_id) {
+//             var masterRole = ""
+//             if (req.user.role_id === 1){
+//                 masterRole = 'teacher'
+//             }else if (req.user.role_id === 2){
+//                 masterRole = 'student' 
+//             }else{
+//                 masterRole = 'mentor'
+//             }
+//             sequelize.query("SELECT * FROM users INNER JOIN comments ON comments.username = users.username WHERE users.username = '" + username + "' ")
+//             .then((results)=>{
                 
                 
-            res.render('home', {
-                publicProfile: '/'+ masterRole + '/'+ req.user.username,
-                user:req.user,
-                fName: req.user.fname,
-                mainUserName:req.user.fname,
-                lName: req.user.lname,
-                mainUser: req.user.username,
-                post: results[0],
-                friendInfo:'',
-                fRole:''
-            })
-        })
-        }else{
-            res.redirect('/login')
-        }
-    }else{
-        res.redirect('/login')
-    }
-})
-router.post("/:userRole/:username", (req, res) => {
-    var username = req.user.username;
-    var comment = req.body.post;
+//             res.render('profile', {
+//                 publicProfile: '/'+ masterRole + '/'+ req.user.username,
+//                 user:req.user,
+//                 fName: req.user.fname,
+//                 mainUserName:req.user.fname,
+//                 lName: req.user.lname,
+//                 mainUser: req.user.username,
+//                 post: results[0],
+//                 friendInfo:'',
+//                 fRole:''
+//             })
+//         })
+//         }else{
+//             res.redirect('/login')
+//         }
+//     }else{
+//         res.redirect('/login')
+//     }
+// })
+// router.post("/:userRole/:username", (req, res) => {
+//     var username = req.user.username;
+//     var comment = req.body.post;
 
-    res.status(204).send();
-    db.comments.create({
-        username: username,
-        comment: comment
-    })
-
+//     res.status(204).send();
+//     db.comments.create({
+//         username: username,
+//         comment: comment
+//     })
+    res.send('This is a test')
 })
 
 module.exports = router
